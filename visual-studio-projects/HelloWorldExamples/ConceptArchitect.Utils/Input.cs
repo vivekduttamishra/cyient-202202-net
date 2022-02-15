6af41e5a5ad07144b2e4692ec99b812950740e76
vivekduttamishra <vivek@conceptarchitect.in>
2022-02-15 12:29:43 +0530

    day#5 update#1

---
+++ visual-studio-projects/HelloWorldExamples/ConceptArchitect.Utils/Input.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptArchitect.Utils
+{
+    public class Input
+    {
+        public static string ReadString(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        public static int ReadInt(string prompt)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+
+            return int.Parse(input);
+        }
+
+
+        public static double ReadDouble(string prompt)
+        {
+            return double.Parse(ReadString(prompt));
+        }
+
+        public static bool ReadBoolean(string prompt)
+        {
+            string answer = ReadString(prompt);
+
+            answer = answer.ToLower(); //convert string to all lower case Yes --> yes   NO --> no
+
+
+            if (answer == "true" || answer == "t" || answer == "yes" || answer == "y" || answer == "ok")
+                return true;
+            else
+                return false;
+
+        }
+
+
+
+    }
+}
---
+++ visual-studio-projects/HelloWorldExamples/Demo07-UserInput/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConceptArchitect.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo07_UserInput
+{
+    class Program
+    {
+
+        static string GetRecommendation(int price, double rating)
+        {
+            if (price < 200)
+                return "Inexpensive: You may read it";
+            else if (rating < 3)
+                return "Bad Reviews: You can ignore it";
+            else if (price < 400 && rating > 4)
+                return "Good Book at Great Price. Do Read It";
+            else if (rating > 4)
+                return "Expensive Book. But you can read it.";
+            else
+                return "Sorry! No Recommendation. Take your own call!";
+        }
+
+
+        static void Main(string[] args)
+        {
+            String bookTitle = Input.ReadString("book title? ");
+            int price = Input.ReadInt("Price? ");
+            double rating = Input.ReadDouble("Rating? ");
+
+            Console.WriteLine("Title:\t" + bookTitle);
+            Console.WriteLine("Price:\tRs {0}",price);
+            Console.WriteLine("Rating:\t{0}/5",rating);
+
+            if(Input.ReadBoolean("Do you need recommendataion?"))
+            {
+                string recommendatation = GetRecommendation(price, rating);
+                Console.WriteLine("here is my recommendation: {0}",recommendatation);
+            }
+
+        }
+    }
+}
