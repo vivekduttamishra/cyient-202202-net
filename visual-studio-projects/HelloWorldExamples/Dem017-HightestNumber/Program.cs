420d89782ef5e0e1f8574c3d96a2ac20bcd67058
vivekduttamishra <vivek@conceptarchitect.in>
2022-02-22 11:13:50 +0530

    day#9 update#1

---
+++ visual-studio-projects/HelloWorldExamples/Dem017-HightestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConceptArchitect.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dem017_HightestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input.ReadInt("How many numbers? ");
+            int max = 0;
+            for(int i=1;i<=n;i++)
+            {
+                int number = Input.ReadInt("Enter Number #" + i + ":");
+
+                //we must assume first entered number as the highest
+                if (i == 1)
+                    max = number;
+
+
+                if (number > max) //if new number is bigger than my assumed max
+                    max = number; //change the assumption.
+            }
+
+            Console.WriteLine("Highest Number was {0}",max);
+        }
+    }
+}
---
+++ visual-studio-projects/HelloWorldExamples/Demo18-Prime01/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConceptArchitect.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo18_Prime01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Input.ReadInt("Enter number?");
+            bool prime = true; //let us assume it is prime
+            for(int i=2;i<number;i++)
+            {
+                if (number % i == 0)
+                {
+                    
+                    prime = false; //ok! my assumption was wrong
+                    break;
+                }               
+            }
+
+            if(prime)
+                Console.WriteLine("{0} is prime", number);
+            else
+                Console.WriteLine("{0} is not prime", number);
+        }
+    }
+}
