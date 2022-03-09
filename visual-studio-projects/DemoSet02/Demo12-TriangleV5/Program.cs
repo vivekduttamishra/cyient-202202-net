2ecded9d221b3ae449187ae635fd1f3a2173c429
vivekduttamishra <vivek@conceptarchitect.in>
2022-03-09 12:27:53 +0530

    day#16 update#2

---
+++ visual-studio-projects/DemoSet02/Demo12-TriangleV5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo12_TriangleV5
+{
+    class Program
+    {
+        public static void TestTriangle(Triangle t, double side1Value)
+        {
+            Console.WriteLine("Triangle Before Change");
+            ShowTriangle(t);
+
+            t[1] = side1Value;
+
+            Console.WriteLine("Triangle After Change");
+            ShowTriangle(t);
+
+            Console.WriteLine();
+        }
+
+        private static void ShowTriangle(Triangle t)
+        {
+            t.Draw();
+            Console.WriteLine("Area:" + t.Area);
+            Console.WriteLine("Perimeter:" + t.Perimeter);
+        }
+
+        static void Main(string[] args)
+        {
+            Triangle t1 = new Triangle(3, 4, 5);
+
+            TestTriangle(t1, 4); //triangle should change to Valid Triangle (4,4,5)
+
+
+            Triangle t2 = new Triangle(5, 5, 5);
+
+            TestTriangle(t2, 12); //triangle should change to Invalid Triangle <12,5,5>
+
+
+            Triangle t3 = new Triangle(3, 4, 12);
+            TestTriangle(t3, 9); //invalid triangle should change to valid Triangle<9,4,12>
+        }
+    }
+}
---
+++ visual-studio-projects/DemoSet02/Demo12-TriangleV5/Triangle.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo12_TriangleV5
+{
+    public class Triangle
+    {
+        double[] sides;  //will hold the sides;
+        public bool Valid { get; private set; } //is autoimplemented property with default get/set logic
+
+        public Triangle(double s1, double s2, double s3)
+        {
+            SetSides(s1, s2, s3);
+        }
+
+        public Triangle(double side)
+        {
+            SetSides(side, side, side); //equlilateral
+        }
+
+        public Triangle()
+        {
+            SetSides(1, 1, 1); //unit triangle
+        }
+
+        private void SetSides(double s1, double s2, double s3)
+        {
+            sides =new double[]{ s1,s2,s3};
+
+            Validate();
+
+        }
+
+        private void Validate()
+        {
+            Valid = sides[0] > 0 && sides[1] > 0 && sides[2] > 0 &&
+                    sides[0] + sides[1] > sides[2] &&
+                    sides[1] + sides[2] > sides[0] &&
+                    sides[2] + sides[0] > sides[1];
+        }
+
+        public double this [ int side ]
+        {
+            get
+            {
+                if (side < 1 || side > 3)
+                    return double.NaN;
+                return sides[side - 1];  //side1 is stored on 0 index, side2 is stored on 1 index 
+            }
+
+            set
+            {
+                if (side < 1 || side > 3)
+                    return;
+                sides[side - 1]=value;  //side1 is stored on 0 index, side2 is stored on 1 index 
+                Validate();
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                if (!Valid)
+                    return double.NaN;
+
+                double p = 0;
+                foreach(double side in sides)
+                    p += side;
+
+                return p;
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                if (!Valid)
+                    return double.NaN;
+
+                double s = Perimeter / 2;
+
+                double x = s;
+
+                foreach (double side in sides)
+                    x *= (s - side);  // x= x*(s-side); --> each loop will havde side= s1, s2, s3
+
+                return Math.Sqrt(x);
+
+            }
+        }
+
+        public void Draw()
+        {
+            ConsoleColor color = ConsoleColor.Blue;
+            String text = "Triangle";
+
+            if (!Valid)
+            {
+                color = ConsoleColor.Red;
+                text = "Invalid Triangle";
+            }
+
+            text = text + "<";
+
+            for (int i = 0; i < 3; i++)
+                text += string.Format("{0}{1} ", sides[i], i < 2 ? "," : ">");
+
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+
+        }
+
+
+
+    }
+}
