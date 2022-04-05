817c9eaff6e68d3a138fe24ba37f3eaef77e6e21
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2022-04-05 12:33:22 +0530

    day#22 update#2

---
+++ visual-studio-projects/DemoSet03/Demo17-Delegate/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo17_Delegate
+{
+    class Calculator
+    {
+        public  void PrintResult(int number1, MathFunction fn, int number2)
+        {
+            int result = fn(number1, number2);
+
+            Console.WriteLine($"{number1} {fn.Method.Name} {number2} = {result}");
+        }
+    }
+}
---
+++ visual-studio-projects/DemoSet03/Demo17-Delegate/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo17_Delegate
+{
+
+    public delegate int MathFunction(int x, int y);
+
+    class Program
+    {
+
+
+        public static int Plus(int x,int y) { return x + y; }
+
+        public static int Minus(int x, int y) { return x - y; }
+
+        static void Main(string[] args)
+        {
+            //DelegateTest01();
+
+            int x = 20;
+            int y = 15;
+
+            var calc = new Calculator();
+
+            calc.PrintResult(x, Plus, y);
+            calc.PrintResult(x, Minus, y);
+
+
+            MathFunction abSq = delegate (int a, int b)
+             {
+                 return (a + b) * (a + b);
+             };
+
+            calc.PrintResult(2, abSq, 3); //25
+
+
+            MathFunction divide = (a, b) => { return a / b; };
+
+            calc.PrintResult(x, divide, y);
+
+
+            MathFunction multiply = (a, b) => a * b;
+
+            calc.PrintResult(x, multiply, y);
+
+
+            calc.PrintResult(x, (a, b) => a % b, y);
+
+
+
+        }
+
+        private static void DelegateTest01()
+        {
+            MathFunction add = new MathFunction(Plus);
+
+            MathFunction sub = Minus;
+
+
+
+
+            Console.WriteLine(add.Invoke(4, 8)); //12
+
+            Console.WriteLine(sub(4, 8));
+        }
+    }
+}
